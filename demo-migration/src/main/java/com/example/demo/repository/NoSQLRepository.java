0b99b5829f0df80f7fc6f6b4d363e655f2e93b71
rjagtap <rjagtap@vmware.com>
2020-04-14 11:46:55 +0530

    demo-migration removed

--- demo-migration/src/main/java/com/example/demo/repository/NoSQLRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Customer;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-public interface NoSQLRepository extends MongoRepository<Customer, String> {
-}
--- demo-migration/src/main/java/com/example/demo/repository/SQLRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface SQLRepository extends CrudRepository<Customer, Long> {
-}
